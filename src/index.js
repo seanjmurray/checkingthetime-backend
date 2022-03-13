e1cea418ea075362925e5b73bbf1b8dfd974e16c
Sean Murray <sean.j.murray@protonmail.com>
2022-03-13 16:10:32 -0700

    basic api functionality added

--- src/index.js
+++ src/index.js
@@ -3,48 +3,12 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-
-
-
 app.use(express.json())
 
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
-
-
-
-
 const Watch = require('./models/watchSchema')
-const note =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-const watch = {
-  title: "Marathon diver",
-  img: "https://i.imgur.com/BVxiejX.jpg",
-  brand: "Marathon",
-  model: "Medium Search and Rescue Auto",
-  caliber: "test caliber",
-  origin: "Canada/Swiss",
-  notes: note,
-};
-const watch1 = {
-    title: "Citizen diver",
-    img: "https://i.imgur.com/GSOYp2e.jpg",
-    brand: "Citizen",
-    model: "Promaster GMT",
-    caliber: "Eco Drive",
-    origin: "Japan",
-    notes: note,
-  };
-  const watch2 = {
-    title: "Seiko 5",
-    img: "https://i.imgur.com/ZOEDvYh.jpg",
-    brand: "Seiko",
-    model: "5 Sport",
-    caliber: "test caliber",
-    origin: "Japan",
-    notes: note,
-  };
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -54,11 +18,26 @@ app.use(function(req, res, next) {
 
 app.route('/api/watches')
     .get((req,res) => {
-        res.send([watch, watch1, watch2,watch, watch1, watch2,watch, watch1, watch2])
+      Watch.find({})
+        .then(dbData => {
+          res.send(dbData)
+        })
+        .catch(err => {
+          console.log(err)
+          res.status(400).send('Database error')
+        })
+        
     })
     .post((req, res) => {
-      console.log(req.body)
-      res.send(req.body)
+      const newWatch = new Watch({...req.body.form})
+      newWatch.save()
+        .then((dbData) => {
+          res.send('uploaded')
+        })
+        .catch(err => {
+          console.log(err)
+          res.status(400).send('Database error')
+        })
       
     })
 
