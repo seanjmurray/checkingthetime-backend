7ac0ccae2ab6201aac3e5aa08b82ab9e1d23dd93
Sean Murray <sean.j.murray@protonmail.com>
2022-03-13 16:46:43 -0700

    change hard coded link

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ var { buildSchema } = require('graphql');
 const Watch = require('./models/watchSchema')
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
