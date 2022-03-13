09172555e382769c4f23f6443a39f5a6f3c08ece
Sean Murray <sean.j.murray@protonmail.com>
2022-03-12 23:19:25 -0800

    start working on routes/ api handling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
+
+
+
+app.use(express.json())
+
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+
+
+
+
+const Watch = require('./models/watchSchema')
 const note =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 const watch = {
@@ -32,17 +47,25 @@ const watch1 = {
   };
 
 app.use(function(req, res, next) {
-    console.log(req)
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
 app.route('/api/watches')
     .get((req,res) => {
-        res.send([watch, watch1, watch2])
+        res.send([watch, watch1, watch2,watch, watch1, watch2,watch, watch1, watch2])
+    })
+    .post((req, res) => {
+      console.log(req.body)
+      res.send(req.body)
+      
     })
 
-app.listen(8080, () => {
-    console.log('I am alive on 8080')
-})
+mongoose.connect(process.env.DB_URL)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log('I am alive on 8080')
+    })
+  })
+  .catch((e) => console.error(e))
